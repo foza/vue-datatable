88b631831a6f4ede627281079272ceffe6bf8462
Fozil Umarov <mr.foza@bk.ru>
2019-06-27 15:11:40 +0500

    add server js

---
+++ server.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var https = require('https')
+var privateKey  = fs.readFileSync('./sslcert/key_uz.key');
+var certificate = fs.readFileSync('./sslcert/key_uz.pem');
+
+var credentials = {key: privateKey, cert: certificate};
+
+const express = require("express")
+const path = require("path")
+const app = express()
+
+app.use(express.static(path.join(__dirname, "dist")))
+
+app.all("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
+
+var httpsServer = https.createServer(credentials, app)
+
+httpsServer.listen(8080)
